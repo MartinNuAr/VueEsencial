825bb3fac92c2ccc0613536ee0655a60d2cfcf0b
Martín Núñez <mnunez@linkbgroup.com>
2023-09-17 21:21:18 -0600

    Se crea primer componente

---
+++ my-project/src/components/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="MyComponent">
+        <h1>Mi componente funciona</h1>
+    </div>    
+</template>
+
+<script>
+    export default {
+        name: 'MyComponent',
+        data (){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
