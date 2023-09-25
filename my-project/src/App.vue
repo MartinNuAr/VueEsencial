aa1f7af66e73bb176d7a9e57f4f21f4e7a613a3a
Martín Núñez <mnunez@linkbgroup.com>
2023-09-24 19:40:28 -0600

    Implementación de Bootstrap 4

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,27 +1,50 @@
 <template>
   <div id="app">
-    <MyComponent />
+    <div class="container">
+      <div class="row">
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Active</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+        </ul>
+      </div>
+      <div class="row">
+        <h1>Prueba Bootstrap</h1>
+      </div>
+      <div class="row">
+        <button type="button" class="btn btn-primary">Primary</button>
+        <button type="button" class="btn btn-secondary">Secondary</button>
+        <button type="button" class="btn btn-success">Success</button>
+        <button type="button" class="btn btn-danger">Danger</button>
+        <button type="button" class="btn btn-warning">Warning</button>
+        <button type="button" class="btn btn-info">Info</button>
+        <button type="button" class="btn btn-light">Light</button>
+        <button type="button" class="btn btn-dark">Dark</button>
+        <button type="button" class="btn btn-link">Link</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import MyComponent from './components/MyComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    MyComponent
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
